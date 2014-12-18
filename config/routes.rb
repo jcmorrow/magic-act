a87f4a29e2593bc26ab071215a4fe7c3c83e2933
Josh Morrow <Josh@storyofstuff.org>
2014-12-17 16:23:48 -0800

    Protecting delayed job as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   get '/admins' => 'admins#index'
-  mount RailsAdmin::Engine => '/admin/', as: 'rails_admin'
   resources :jobs
   get '/job/:id/count' => 'jobs#queryCount'
   post '/job/:id/run' => 'jobs#run'
   get '/download/:sub_job_id/:file_type', to: 'downloads#secureGet', as: 'secure_get'
-  match "/delayed_job" => DelayedJobWeb, :anchor => false, via: [:get, :post]
+#  match "/delayed_job" => DelayedJobWeb, :anchor => false, via: [:get, :post]
 
   get '/object_rules/explore' => 'object_rules#explore'
 
@@ -14,5 +13,13 @@ Rails.application.routes.draw do
   resources :object_rules do
     resources :field_rules
   end
+
+
+  #DELAYEDJOBWEB
+  authenticated :admin do
+    mount DelayedJobWeb, :at => "/delayed_job"
+  end
+  #RAILS ADMIN
+  mount RailsAdmin::Engine => '/admin/', as: 'rails_admin'
 
 end
