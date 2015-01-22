5d300dc70f286b992640e563a30971aed4b652f7
Josh Morrow <Josh@storyofstuff.org>
2015-01-22 13:05:49 -0800

    Got it to run correctly once, but it's still not recurring

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -4,13 +4,13 @@ module Recurring
 	  run_every 30.minutes
 	  run_at (Time.now + 1.minute).strftime('%l:%M%P')
 	  logger.info "job scheduled for "<<(Time.now + 1.minute).strftime('%l:%M%P')
-	  #timezone 'US/Pacific'
+	  #timezone 'US/Pacific' #this line is causing some issues on the server.
 	  queue 'ETL-jobs'
 	  def perform
 	  	logger.info "Job should run"
-	    EtlJobs.all.each do |job|
+	    Jobs.all.each do |job|
 	    	job.run
-	    	puts "Job should be running"
+	    	logger.info "Job is running"
 	    end
 	  end
 	end
