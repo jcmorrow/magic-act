5bc5abf30ea87f04edf486cd944544caf0651975
Josh Morrow <Josh@storyofstuff.org>
2015-01-22 12:35:58 -0800

    Just messing around with Delayed Job, trying tofigure out why it isn't working

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -3,11 +3,14 @@ module Recurring
 	  include Delayed::RecurringJob
 	  run_every 30.minutes
 	  run_at (Time.now + 1.minute).strftime('%l:%M%P')
-	  timezone 'US/Pacific'
+	  logger.info "job scheduled for "<<(Time.now + 1.minute).strftime('%l:%M%P')
+	  #timezone 'US/Pacific'
 	  queue 'ETL-jobs'
 	  def perform
+	  	logger.info "Job should run"
 	    EtlJobs.all.each do |job|
 	    	job.run
+	    	puts "Job should be running"
 	    end
 	  end
 	end
